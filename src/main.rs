5f6de4ee99e9c58dab1ee9a80d649b39800d0f88
Tomas Salgado <91388965+tomas-salgado@users.noreply.github.com>
2024-12-26 01:16:22 -0600

    add SET and GET functionality

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::Write;
 use std::io::Read;
+use std::collections::HashMap;
+
 fn main() {
 fn handle_client(mut stream: TcpStream) {
     println!("New client connected: {}", stream.peer_addr().unwrap());
 
     let mut buffer = [0; 1024];
+    let mut data = HashMap::new();
     loop {
         match stream.read(&mut buffer) {
             Ok(n) => {
                 if n == 0 {
                     break;
                 }
-                println!("Received message: {}", String::from_utf8_lossy(&buffer[..n]));
-                stream.write(&buffer[..n]).unwrap();
+
+                let request = String::from_utf8_lossy(&buffer[..n]).to_string();
+                let parts: Vec<String> = request.split_whitespace().map(String::from).collect();
+
+                match parts.get(0) {
+                    Some(cmd) if cmd == "SET" && parts.len() >= 3 => {
+                        let key = parts[1].clone();
+                        let value = parts[2].clone();
+                        data.insert(key, value);
+                        stream.write(b"OK\n").unwrap();
+                    }
+                    Some (cmd) if cmd == "GET" && parts.len() >= 2 => {
+                        let key = parts[1].clone();
+                        let value = data.get(&key);
+                        let response = match value {
+                            Some(v) => format!("{}\n", v),
+                            None => "nil\n".to_string(),
+                        };
+                        stream.write(response.as_bytes()).unwrap();
+                    }
+                    _=> {
+                        stream.write(b"ERROR: Unknown command\n").unwrap();
+                    }
+                }
             }
             Err(e) => {
                 eprintln!("Error reading from stream: {}", e);
